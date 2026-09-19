2b2c2f00212952bae4fee289c69aa5ccc4b5a675
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2026-09-19 13:42:06 +0200

    chore: configure ESLint and Prettier (T002)

---
+++ eslint.config.js
@@ -0,0 +1,41 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-config-prettier";
+
+export default [
+  {
+    ignores: [".output/", ".wxt/", "node_modules/", "dist/", "coverage/", "playwright-report/", "test-results/"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: { project: "./tsconfig.json" },
+      globals: {
+        chrome: "readonly",
+        browser: "readonly",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...tseslint.configs.strict.rules,
+      ...reactHooks.configs.recommended.rules,
+      "no-undef": "off",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
+  {
+    files: ["tests/**/*.{ts,tsx}"],
+    rules: {
+      "@typescript-eslint/no-non-null-assertion": "off",
+    },
+  },
+  prettier,
+];
